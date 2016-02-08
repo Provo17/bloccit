7e5426440cd15bad98e07dc7b76a338239e8fb1a
provo17 <provo17.gp@gmail.com>
2016-02-08 23:37:34 +0000

    app/views/welcome/faq.html.erb

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 #1
 RSpec.describe WelcomeController, type: :controller do
+    
     describe "GET index" do
         it "renders the index template" do
 #2
@@ -14,6 +15,13 @@ RSpec.describe WelcomeController, type: :controller do
      it "renders the about template" do
        get :about
        expect(response).to render_template("about")
+     end
+   end  
+   
+   describe "GET faq" do
+     it "renders the faq template" do
+       get :faq
+       expect(response).to render_template("faq")
      end
    end  
 end
