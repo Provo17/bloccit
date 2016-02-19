8cfd4db6401ee81ea79a1e5970539c6481b1b4e6
provo17 <provo17.gp@gmail.com>
2016-02-19 10:56:05 +0000

    authentication_checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
-resources :topics do
+   resources :topics do
  # #34
      resources :posts, except: [:index]
    end
+   
+   
+   resources :users, only: [:new, :create]
+   
 
 get 'about' => 'welcome#about'
 
