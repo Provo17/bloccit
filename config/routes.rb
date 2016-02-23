3064768a91cd4a60ce8739d356ded4c12add69f9
provo17 <provo17.gp@gmail.com>
2016-02-23 02:14:20 +0000

    signing up assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
-resources :topics do
- # #34
-     resources :posts, except: [:index]
-   end
+    resources :topics do
+        resources :posts, except: [:index]
+    end
    
-   resources :users, only: [:new, :create]
+    resources :users, only: [:new, :create]
 
-get 'about' => 'welcome#about'
-get 'faq' => 'welcome#faq'
 
-  root 'welcome#index'
+    get 'about' => 'welcome#about'
+    get 'faq' => 'welcome#faq'
+    post 'users/confirm' => 'users#confirm'
+    
+    root 'welcome#index'
 end
