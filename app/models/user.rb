8fd55177d2f0ced0c324d5f6ddd80d40c8aaccde
provo17 <provo17.gp@gmail.com>
2016-02-29 01:58:37 +0000

    added authorizations for members, guest, and admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
     
     has_many :posts
+    
     before_save { self.email = email.downcase }
-
+    before_save { self.role ||= :member }
 
     EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -18,5 +19,7 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 100 },
             format: { with: EMAIL_REGEX }
 
-   has_secure_password
+    has_secure_password
+   
+    enum role: [:member, :admin]
 end
