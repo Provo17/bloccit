755ba4924aa649a223bcc3ff2dd30270ed34afd5
provo17 <provo17.gp@gmail.com>
2016-03-03 19:40:37 +0000

    added the ability to creates labels for topics and posts

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -6,8 +6,11 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
  
     it { is_expected.to have_many(:posts) }
+
+    it { is_expected.to have_many(:labelings) }
  
- # #1
+    it { is_expected.to have_many(:labels).through(:labelings) }
+ 
    describe "attributes" do
      it "responds to name" do
        expect(topic).to respond_to(:name)
@@ -21,7 +24,6 @@ RSpec.describe Topic, type: :model do
        expect(topic).to respond_to(:public)
      end
  
- # #2
      it "is public by default" do
        expect(topic.public).to be(true)
      end
