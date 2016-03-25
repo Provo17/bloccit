dc8ace3393ecca1c953506dcc2fa858cfa3e0f51
provo17 <provo17.gp@gmail.com>
2016-03-25 20:32:48 +0000

    added api to send post related data

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,11 @@ Rails.application.routes.draw do
    namespace :api do
      namespace :v1 do
        resources :users, only: [:index, :show, :create, :update]
-       resources :topics, except: [:edit, :new]       
+       resources :topics, except: [:edit, :new]     
+       
+       resources :topics do
+           resources :posts, only:[:create, :update, :destroy]
+       end
      end
    end    
 end
